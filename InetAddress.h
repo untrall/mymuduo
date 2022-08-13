e358240b7c7727249f694d144b307cbd0e031673
untrall <1035131072@qq.com>
2022-08-13 19:13:05 +0800

    新增InetAddress

---
+++ InetAddress.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <netinet/in.h>
+#include <string>
+
+class InetAddress
+{
+public:
+    explicit InetAddress(uint16_t port, std::string ip = "127.0.0.1");
+    explicit InetAddress(const sockaddr_in &addr)
+        : addr_(addr)
+    {}
+    std::string toIp() const;
+    std::string toIpPort() const;
+    uint16_t toPort() const;
+    const sockaddr_in* getSocketAddr() const {return &addr_;}
+private:
+    sockaddr_in addr_;
+};
